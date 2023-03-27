f65302f23b49542ef702e16586e666d234167592
ThamerAyachi <thamerjak3@gmail.com>
2023-03-27 01:07:12 +0100

    add tasks

--- models/place.py
+++ models/place.py
@@ -9,6 +9,17 @@ from sqlalchemy.orm import relationship
 from os import getenv
 
 
+association_table = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True,
+                                 nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True,
+                                 nullable=False))
+
+
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
@@ -25,3 +36,35 @@ class Place(BaseModel, Base):
     longitude = Column(Float)
 
     amenity_ids = []
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+
+        @property
+        def reviews(self):
+            """get a list of all related review instances
+            with review.places_id = to the current place.id
+            """
+            reviews_list = []
+
+            for review in list(models.storage.all(Review).values()):
+                if review.place_id == self.id:
+                    reviews_list.append(review)
+            return reviews_list
+
+        @property
+        def amenities(self):
+            """get a list of all related amenity instances
+            with amenity.id = to the current self amenity ids
+            """
+            amenities_list = []
+
+            for amenity in list(models.storage.all(Amenity).values()):
+                if amenity.id in self.amenity_ids:
+                    amenities_list.append(amenity)
+            return amenities_list
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Function that handles append method for adding an amenity"""
+            if type(obj) == Amenity:
+                self.amenities_ids.append(obj.id)
